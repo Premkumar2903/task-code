fb1a6f04ecfe9403fb5430d828ec7919846152a7
PREMKUMAR M <prem29mahendran@gmail.com>
2026-02-07 00:16:44 +0530

    task 4

---
+++ JavaScript/task 4/1.functionAdd.js
@@ -0,0 +1,21 @@
+
+
+function Add(a,b) {
+    console.log(a+b)
+}
+
+Add(5,5)
+
+
+function multiply () {
+    console.log(5*5)
+}
+
+multiply()
+
+
+ const Sub = (a,b) =>{
+    console.log(a-b) 
+ }
+
+Sub(8,1)
