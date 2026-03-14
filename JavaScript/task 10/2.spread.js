5a121872f77b1554764097c8b989e1c3f6d110a3
PREMKUMAR M <prem29mahendran@gmail.com>
2026-03-14 13:02:03 +0530

    rearrange

--- JavaScript/task 10/2.spread.js
+++
@@ -1,7 +0,0 @@
-const arr = [300,500,700];
-
-
-//spread operator 
-arr2 = [100,...arr]  //creates new separate array
-
-console.log(arr2);
--- JavaScript/task 12/2.static.js
+++
@@ -1,9 +0,0 @@
-class counter {
-    static count = 0;                       //field
-    
-    static add (value){
-        console.log(counter.count += value)  //method
-    }
-}
-
-counter.add(4)                  
