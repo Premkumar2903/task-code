c5b859637c92e81dcaf5e956cd62a585d7f53604
PREMKUMAR M <prem29mahendran@gmail.com>
2026-03-29 23:29:33 +0530

    task 1

---
+++ Sql/task-01.sql
@@ -0,0 +1,18 @@
+create table schoolDB(
+	id int,
+    name_stud varchar(100),
+    age int,
+    grade varchar(2)
+);
+
+insert into schoolDB(id,name_stud,age,grade) 
+
+values
+(1,'prem',22,'A'),
+(1,'abdul',22,'A'),
+(1,'ajay',22,'A'),
+(1,'arun',22,'A'),
+(1,'loki',22,'A');
+
+select*from schooldb
+
