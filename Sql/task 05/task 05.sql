bfee3e423eaf7c9024ed434b9b8aa2407b11fd1e
Premkumar <prem29mahendran@gmail.com>
2026-04-03 12:21:39 +0530

    task 5

--- Sql/task 05/task 05.sql
+++ Sql/task 05/task 05.sql
@@ -1,5 +1,3 @@
-create database products;
-
 create table orders (
 	product_name varchar(50) unique,
     price int 
@@ -8,6 +6,14 @@ create table orders (
 insert into orders(product_name,price) values ('shoes', 1500),('t-shirt',500),('watch',2000) , ('jeans', 800),('bag', 800),('Badminton Racquet', 1000);
 select * from orders;
 
+select count(*) as total_orders from orders;     -- counts orders
 
-select count(*) as orders
+select sum(price) as total_price from orders;    -- total price sum of all
+
+select avg(price) as average_price from orders;   -- avg price
+
+select product_name, sum(price) as total_sales from orders group by product_name; -- groupby
+
+select max(price) as max_price , min(price) as min_price from orders;
+
 
